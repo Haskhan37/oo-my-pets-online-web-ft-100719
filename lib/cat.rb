7cf57a86c4800e68d12a36f0deeca6cf185f0cc0
Hasan Khan <hassankhankabacha@gmail.com>
2019-10-23 19:56:05 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,8 +3,8 @@ class Cat
   attr_reader :name
   attr_accessor :mood
 
-  def initialize(name, mood = "nervous")
-    @mood = mood
+  def initialize(name)
+    @mood = "nervous"
     @name = name
   end
 end
